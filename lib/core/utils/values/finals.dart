26e05cc221446f1124b4aead90c421f603b9518c
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-08-07 15:00:22 -0300

    feat: adds finals value

---
+++ lib/core/utils/values/finals.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+final outlineBorder = OutlineInputBorder(
+  borderRadius: BorderRadius.circular(10),
+  borderSide: const BorderSide(color: Colors.white),
+);
+
+final errorBorder = OutlineInputBorder(
+  borderRadius: BorderRadius.circular(10),
+  borderSide: const BorderSide(color: Colors.red),
+);
