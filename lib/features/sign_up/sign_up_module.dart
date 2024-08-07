209f9db12f566de5e896529dd8fccb4c9578135b
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-08-07 15:59:12 -0300

    feat: adds binds to sign up module

--- lib/features/sign_up/sign_up_module.dart
+++ lib/features/sign_up/sign_up_module.dart
@@ -1,10 +1,19 @@
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:keener_challenge/features/sign_up/presenter/sign_up_page.dart';
+import 'package:keener_challenge/features/sign_up/domain/repository/sign_up_repository.dart';
+import 'package:keener_challenge/features/sign_up/domain/usecase/sign_up_usecase.dart';
+import 'package:keener_challenge/features/sign_up/external/sign_up_datasource_impl.dart';
+import 'package:keener_challenge/features/sign_up/infra/datasource/sign_up_datasource.dart';
+import 'package:keener_challenge/features/sign_up/infra/repository/sign_in_repository_impl.dart';
+import 'package:keener_challenge/features/sign_up/presenter/controllers/sign_up_controller.dart';
+import 'package:keener_challenge/features/sign_up/presenter/pages/sign_up_page.dart';
 
 class SignUpModule extends Module {
   @override
   void binds(Injector i) {
-    // TODO: implement binds
+    i.add<SignUpDatasource>(SignUpDataSourceImpl.new);
+    i.add<SignUpRepository>(SignUpRepositoryImpl.new);
+    i.add<SignUpUsecase>(SignUpUsecaseImpl.new);
+    i.add<SignUpController>(SignUpController.new);
     super.binds(i);
   }
 
@@ -13,6 +22,7 @@ class SignUpModule extends Module {
     r.child(
       '/',
       child: (context) => const SignUpPage(),
+      transition: TransitionType.rightToLeft,
     );
     super.routes(r);
   }
