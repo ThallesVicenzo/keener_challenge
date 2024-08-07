429b322ab1a9b63aa30ba66e4192232bbfe24be1
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-08-07 15:58:38 -0300

    feat: adds sign up controller

---
+++ lib/features/sign_up/presenter/controllers/sign_up_controller.dart
@@ -0,0 +1,7 @@
+import 'package:mobx/mobx.dart';
+
+part 'sign_up_controller.g.dart';
+
+class SignUpController = _SignUpController with _$SignUpController;
+
+abstract class _SignUpController with Store {}
