42663c62dc4171816b113cc39c10d37f14b78a8f
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-08-07 09:41:50 -0300

    feat: adds welcome on app module

--- lib/app_module.dart
+++ lib/app_module.dart
@@ -1,3 +1,20 @@
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:keener_challenge/core/routes/named_routes.dart';
+import 'package:keener_challenge/features/welcome/presenter/welcome_module.dart';
 
-class AppModule extends Module {}
+class AppModule extends Module {
+  @override
+  void binds(Injector i) {
+    // TODO: implement binds
+    super.binds(i);
+  }
+
+  @override
+  void routes(RouteManager r) {
+    r.module(
+      NamedRoutes.welcome.route,
+      module: WelcomeModule(),
+    );
+    super.routes(r);
+  }
+}
