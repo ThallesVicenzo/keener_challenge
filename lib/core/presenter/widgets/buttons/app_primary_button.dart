fa112c9cfdb13eeb794c423b8e0cb3685bc74216
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-08-07 11:41:28 -0300

    feat: adds AppPrimaryButton

---
+++ lib/core/presenter/widgets/buttons/app_primary_button.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+import '../text/app_text.dart';
+
+class AppPrimaryButton extends StatelessWidget {
+  const AppPrimaryButton({
+    super.key,
+    required this.title,
+    required this.onTap,
+    this.sufixIconVisibility,
+  });
+
+  final String title;
+  final void Function() onTap;
+  final bool? sufixIconVisibility;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 14),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(14),
+          color: Colors.deepPurple,
+        ),
+        height: 52,
+        width: double.infinity,
+        child: Stack(
+          children: [
+            Align(
+              alignment: Alignment.center,
+              child: AppText(
+                title,
+                fontSize: FontSize.medium,
+                style: MyTextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+            Visibility(
+              visible: sufixIconVisibility ?? true,
+              child: const Align(
+                alignment: Alignment.centerRight,
+                child: Icon(
+                  Icons.arrow_forward,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
