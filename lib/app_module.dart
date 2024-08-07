e0799dc9fa8ab6b0c7656084f7c5cf35393d3cc5
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-08-07 15:59:35 -0300

    feat: adds firebase auth bind on app module

--- lib/app_module.dart
+++ lib/app_module.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:keener_challenge/core/routes/named_routes.dart';
 import 'package:keener_challenge/features/sign_up/sign_up_module.dart';
@@ -6,7 +7,7 @@ import 'package:keener_challenge/features/welcome/welcome_module.dart';
 class AppModule extends Module {
   @override
   void binds(Injector i) {
-    // TODO: implement binds
+    i.addSingleton<FirebaseAuth>(() => FirebaseAuth.instance);
     super.binds(i);
   }
 
