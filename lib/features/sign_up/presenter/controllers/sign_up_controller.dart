eb06ad613b30f7040d0d0d93b1a1c267381a724d
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-08-08 15:01:38 -0300

    feat: adds sign up controller

--- lib/features/sign_up/presenter/controllers/sign_up_controller.dart
+++ lib/features/sign_up/presenter/controllers/sign_up_controller.dart
@@ -1,7 +1,117 @@
+import 'package:email_validator/email_validator.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:keener_challenge/core/page_state.dart';
+import 'package:keener_challenge/core/routes/named_routes.dart';
+import 'package:keener_challenge/core/secure_storage/keys/secure_storage_keys.dart';
+import 'package:keener_challenge/core/secure_storage/secure_storage.dart';
+import 'package:keener_challenge/features/sign_up/domain/usecase/sign_up_usecase.dart';
 import 'package:mobx/mobx.dart';
 
 part 'sign_up_controller.g.dart';
 
-class SignUpController = _SignUpController with _$SignUpController;
+class SignUpController = SignUpControllerBase with _$SignUpController;
 
-abstract class _SignUpController with Store {}
+abstract class SignUpControllerBase with Store {
+  SignUpControllerBase({required this.usecase, required this.secureStorage});
+
+  final SignUpUsecase usecase;
+  final SecureStorage secureStorage;
+
+  final state = ValueNotifier<PageState<UserCredential>>(InitialState());
+
+  @observable
+  bool obscurePassword = true;
+
+  @observable
+  bool obscureConfirmPassword = true;
+
+  final formKeys = <GlobalKey<FormState>>[
+    GlobalKey<FormState>(),
+    GlobalKey<FormState>(),
+    GlobalKey<FormState>(),
+    GlobalKey<FormState>(),
+  ];
+
+  final controllers = <TextEditingController>[
+    TextEditingController(),
+    TextEditingController(),
+    TextEditingController(),
+    TextEditingController(),
+  ];
+
+  @action
+  void changePasswordVisibility() {
+    obscurePassword = !obscurePassword;
+  }
+
+  @action
+  void changeConfirmPasswordVisibility() {
+    obscureConfirmPassword = !obscureConfirmPassword;
+  }
+
+  String? validateUsername(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Can\'t be empty!';
+    } else if (value.length > 10) {
+      return 'Username can\'t have more than 10 characters.';
+    }
+
+    return null;
+  }
+
+  String? validateEmail(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Can\'t be empty!';
+    } else if (EmailValidator.validate(value) == false) {
+      return 'Please type a valid email.';
+    }
+
+    return null;
+  }
+
+  String? validatePassword(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Can\'t be empty!';
+    } else if (value.length < 6) {
+      return 'Password can not have less than six characters.';
+    }
+
+    return null;
+  }
+
+  String? validateConfirmPassword(String? value) {
+    if (controllers[2].text != controllers[3].text) {
+      return 'Passwords must be equal!';
+    }
+    return null;
+  }
+
+  @action
+  Future<void> createAccount() async {
+    if (formKeys[0].currentState!.validate() &&
+        formKeys[1].currentState!.validate() &&
+        formKeys[2].currentState!.validate() &&
+        formKeys[3].currentState!.validate()) {
+      state.value = LoadingState();
+
+      final result = await usecase.createAccount(
+        email: controllers[1].text,
+        password: controllers[2].text,
+      );
+
+      result.fold(
+        (l) => state.value = ErrorState(error: l),
+        (r) async {
+          await secureStorage.write(
+            key: SecureStorageKeys.username.key,
+            value: controllers[0].text,
+          );
+          Modular.to.navigate(NamedRoutes.home.route);
+          state.value = SuccessState(data: r);
+        },
+      );
+    }
+  }
+}
--- lib/features/sign_up/presenter/controllers/sign_up_controller.g.dart
+++ lib/features/sign_up/presenter/controllers/sign_up_controller.g.dart
@@ -8,11 +8,78 @@ part of 'sign_up_controller.dart';
 
 // ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic, no_leading_underscores_for_local_identifiers
 
-mixin _$SignUpController on _SignUpController, Store {
+mixin _$SignUpController on SignUpControllerBase, Store {
+  late final _$obscurePasswordAtom =
+      Atom(name: 'SignUpControllerBase.obscurePassword', context: context);
+
+  @override
+  bool get obscurePassword {
+    _$obscurePasswordAtom.reportRead();
+    return super.obscurePassword;
+  }
+
+  @override
+  set obscurePassword(bool value) {
+    _$obscurePasswordAtom.reportWrite(value, super.obscurePassword, () {
+      super.obscurePassword = value;
+    });
+  }
+
+  late final _$obscureConfirmPasswordAtom = Atom(
+      name: 'SignUpControllerBase.obscureConfirmPassword', context: context);
+
+  @override
+  bool get obscureConfirmPassword {
+    _$obscureConfirmPasswordAtom.reportRead();
+    return super.obscureConfirmPassword;
+  }
+
+  @override
+  set obscureConfirmPassword(bool value) {
+    _$obscureConfirmPasswordAtom
+        .reportWrite(value, super.obscureConfirmPassword, () {
+      super.obscureConfirmPassword = value;
+    });
+  }
+
+  late final _$createAccountAsyncAction =
+      AsyncAction('SignUpControllerBase.createAccount', context: context);
+
+  @override
+  Future<void> createAccount() {
+    return _$createAccountAsyncAction.run(() => super.createAccount());
+  }
+
+  late final _$SignUpControllerBaseActionController =
+      ActionController(name: 'SignUpControllerBase', context: context);
+
+  @override
+  void changePasswordVisibility() {
+    final _$actionInfo = _$SignUpControllerBaseActionController.startAction(
+        name: 'SignUpControllerBase.changePasswordVisibility');
+    try {
+      return super.changePasswordVisibility();
+    } finally {
+      _$SignUpControllerBaseActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void changeConfirmPasswordVisibility() {
+    final _$actionInfo = _$SignUpControllerBaseActionController.startAction(
+        name: 'SignUpControllerBase.changeConfirmPasswordVisibility');
+    try {
+      return super.changeConfirmPasswordVisibility();
+    } finally {
+      _$SignUpControllerBaseActionController.endAction(_$actionInfo);
+    }
+  }
+
   @override
   String toString() {
     return '''
-
+obscurePassword: ${obscurePassword},
+obscureConfirmPassword: ${obscureConfirmPassword}
     ''';
   }
 }
