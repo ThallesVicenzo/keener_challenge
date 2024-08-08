25337892a67d49fec159dfb6ce26a78cb937072e
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-08-08 16:03:11 -0300

    feat: updates login module with integration

--- lib/features/login/login_module.dart
+++ lib/features/login/login_module.dart
@@ -1,10 +1,23 @@
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:keener_challenge/features/login/domain/repository/login_repository.dart';
+import 'package:keener_challenge/features/login/domain/usecase/login_usecase.dart';
+import 'package:keener_challenge/features/login/external/login_datasource_impl.dart';
+import 'package:keener_challenge/features/login/infra/datasource/login_datasource.dart';
+import 'package:keener_challenge/features/login/infra/repository/login_repository_impl.dart';
+import 'package:keener_challenge/features/login/presenter/controllers/login_controller.dart';
 import 'package:keener_challenge/features/login/presenter/pages/login_page.dart';
 
 class LoginModule extends Module {
   @override
   void binds(Injector i) {
-    // TODO: implement binds
+    i.addInstance<LoginDatasource>(
+      LoginDataSourceImpl(
+        Modular.get(key: 'auth'),
+      ),
+    );
+    i.add<LoginRepository>(LoginRepositoryImpl.new);
+    i.add<LoginUsecase>(LogincaseImpl.new);
+    i.add<LoginController>(LoginController.new);
     super.binds(i);
   }
 
@@ -12,7 +25,9 @@ class LoginModule extends Module {
   void routes(RouteManager r) {
     r.child(
       '/',
-      child: (context) => const LoginPage(),
+      child: (context) => LoginPage(
+        controller: context.read<LoginController>(),
+      ),
       transition: TransitionType.rightToLeft,
     );
     super.routes(r);
