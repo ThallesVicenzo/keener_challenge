025e4a79ac13309b0da03ed0057d85cb48ba9d88
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-08-08 14:31:00 -0300

    feat: adds keys to get global injectors

--- lib/app_module.dart
+++ lib/app_module.dart
@@ -1,13 +1,22 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:keener_challenge/core/routes/named_routes.dart';
+import 'package:keener_challenge/core/secure_storage/secure_storage.dart';
+import 'package:keener_challenge/core/secure_storage/secure_storage_impl.dart';
 import 'package:keener_challenge/features/sign_up/sign_up_module.dart';
 import 'package:keener_challenge/features/welcome/welcome_module.dart';
 
 class AppModule extends Module {
   @override
   void binds(Injector i) {
-    i.addSingleton<FirebaseAuth>(() => FirebaseAuth.instance);
+    i.add<FirebaseAuth>(
+      () => FirebaseAuth.instance,
+      key: 'auth',
+    );
+    i.add<SecureStorage>(
+      SecureStorageImpl.new,
+      key: 'storage',
+    );
     super.binds(i);
   }
 
