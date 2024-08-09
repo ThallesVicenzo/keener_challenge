f350545ef8f9b2dcafca1015993202fd92d143c8
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-08-09 15:51:24 -0300

    feat: injects firestore and adds default error page route

--- lib/app_module.dart
+++ lib/app_module.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:keener_challenge/core/routes/named_routes.dart';
@@ -8,12 +9,18 @@ import 'package:keener_challenge/features/login/login_module.dart';
 import 'package:keener_challenge/features/sign_up/sign_up_module.dart';
 import 'package:keener_challenge/features/welcome/welcome_module.dart';
 
+import 'core/presenter/pages/default_erro_page.dart';
+
 class AppModule extends Module {
   @override
   void binds(Injector i) {
     i.add<FirebaseAuth>(
       () => FirebaseAuth.instance,
       key: 'auth',
+    );
+    i.add<FirebaseFirestore>(
+      () => FirebaseFirestore.instance,
+      key: 'firestore',
     );
     i.add<SecureStorage>(
       SecureStorageImpl.new,
@@ -39,6 +46,21 @@ class AppModule extends Module {
     r.module(
       NamedRoutes.home.route,
       module: HomeModule(),
+    );
+
+    r.child(
+      NamedRoutes.defaultError.route,
+      child: (context) {
+        final erroParams = r.args.data is ErrorPageParams
+            ? (r.args.data as ErrorPageParams)
+            : const ErrorPageParams();
+        return DefaultErrorPage(
+          params: erroParams,
+          onBackStart: () {
+            Modular.to.navigate(NamedRoutes.home.route);
+          },
+        );
+      },
     );
     super.routes(r);
   }
