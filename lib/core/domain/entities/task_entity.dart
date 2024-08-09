d35aaea4172b73a68754fab73dc56ca74681c28d
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-08-09 14:36:38 -0300

    feat: adds task entity

---
+++ lib/core/domain/entities/task_entity.dart
@@ -0,0 +1,11 @@
+class TaskEntity {
+  final bool isCompleted;
+  final String title;
+  final String description;
+
+  TaskEntity({
+    required this.isCompleted,
+    required this.title,
+    required this.description,
+  });
+}
