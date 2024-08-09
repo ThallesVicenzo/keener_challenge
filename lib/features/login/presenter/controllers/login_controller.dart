8bdb94bfb32a76b6a702d0c2c7e8c38e377ddcb8
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-08-09 10:46:22 -0300

    feat: creates login controller

---
+++ lib/features/login/presenter/controllers/login_controller.dart
@@ -0,0 +1,93 @@
+import 'package:email_validator/email_validator.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:keener_challenge/core/page_state.dart';
+import 'package:keener_challenge/core/presenter/widgets/text/app_text.dart';
+import 'package:keener_challenge/core/routes/named_routes.dart';
+import 'package:keener_challenge/features/login/domain/usecase/login_usecase.dart';
+import 'package:mobx/mobx.dart';
+
+part 'login_controller.g.dart';
+
+class LoginController = LoginControllerBase with _$LoginController;
+
+abstract class LoginControllerBase with Store {
+  final state = ValueNotifier<PageState<UserCredential>>(InitialState());
+
+  LoginControllerBase({required this.usecase});
+
+  final LoginUsecase usecase;
+
+  @observable
+  bool obscurePassword = true;
+
+  final formKeys = <GlobalKey<FormState>>[
+    GlobalKey<FormState>(),
+    GlobalKey<FormState>(),
+  ];
+
+  final controllers = <TextEditingController>[
+    TextEditingController(),
+    TextEditingController(),
+  ];
+
+  @action
+  void changePasswordVisibility() {
+    obscurePassword = !obscurePassword;
+  }
+
+  String? validateEmail(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Can\'t be empty!';
+    } else if (EmailValidator.validate(value) == false) {
+      return 'Please type a valid email.';
+    }
+
+    return null;
+  }
+
+  String? validatePassword(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Can\'t be empty!';
+    } else if (value.length < 6) {
+      return 'Password can not have less than six characters.';
+    }
+
+    return null;
+  }
+
+  @action
+  Future<void> login(context) async {
+    if (formKeys[0].currentState!.validate() &&
+        formKeys[1].currentState!.validate()) {
+      state.value = LoadingState();
+
+      final result = await usecase.login(
+        email: controllers[0].text,
+        password: controllers[1].text,
+      );
+
+      result.fold(
+        (l) {
+          state.value = ErrorState(error: l);
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: AppText(
+                l.message ?? 'Invalid user, please try again.',
+                style: MyTextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              backgroundColor: Colors.deepPurple,
+            ),
+          );
+        },
+        (r) async {
+          Modular.to.navigate(NamedRoutes.home.route);
+          state.value = SuccessState(data: r);
+        },
+      );
+    }
+  }
+}
---
+++ lib/features/login/presenter/controllers/login_controller.g.dart
@@ -0,0 +1,56 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'login_controller.dart';
+
+// **************************************************************************
+// StoreGenerator
+// **************************************************************************
+
+// ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic, no_leading_underscores_for_local_identifiers
+
+mixin _$LoginController on LoginControllerBase, Store {
+  late final _$obscurePasswordAtom =
+      Atom(name: 'LoginControllerBase.obscurePassword', context: context);
+
+  @override
+  bool get obscurePassword {
+    _$obscurePasswordAtom.reportRead();
+    return super.obscurePassword;
+  }
+
+  @override
+  set obscurePassword(bool value) {
+    _$obscurePasswordAtom.reportWrite(value, super.obscurePassword, () {
+      super.obscurePassword = value;
+    });
+  }
+
+  late final _$loginAsyncAction =
+      AsyncAction('LoginControllerBase.login', context: context);
+
+  @override
+  Future<void> login(dynamic context) {
+    return _$loginAsyncAction.run(() => super.login(context));
+  }
+
+  late final _$LoginControllerBaseActionController =
+      ActionController(name: 'LoginControllerBase', context: context);
+
+  @override
+  void changePasswordVisibility() {
+    final _$actionInfo = _$LoginControllerBaseActionController.startAction(
+        name: 'LoginControllerBase.changePasswordVisibility');
+    try {
+      return super.changePasswordVisibility();
+    } finally {
+      _$LoginControllerBaseActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  String toString() {
+    return '''
+obscurePassword: ${obscurePassword}
+    ''';
+  }
+}
