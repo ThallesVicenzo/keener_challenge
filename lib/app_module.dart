b91b3e59d4dfd66dfbf99dcea08c310ba46ebb6a
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-08-10 15:44:22 -0300

    feat: injects edit and add modules on app module

--- lib/app_module.dart
+++ lib/app_module.dart
@@ -4,6 +4,8 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'package:keener_challenge/core/routes/named_routes.dart';
 import 'package:keener_challenge/core/secure_storage/secure_storage.dart';
 import 'package:keener_challenge/core/secure_storage/secure_storage_impl.dart';
+import 'package:keener_challenge/features/add_task/add_task_module.dart';
+import 'package:keener_challenge/features/edit_task/edit_task_module.dart';
 import 'package:keener_challenge/features/home/home_module.dart';
 import 'package:keener_challenge/features/login/login_module.dart';
 import 'package:keener_challenge/features/sign_up/sign_up_module.dart';
@@ -46,6 +48,14 @@ class AppModule extends Module {
     r.module(
       NamedRoutes.home.route,
       module: HomeModule(),
+    );
+    r.module(
+      NamedRoutes.addTask.route,
+      module: AddTaskModule(),
+    );
+    r.module(
+      NamedRoutes.editTask.route,
+      module: EditTaskModule(),
     );
 
     r.child(
