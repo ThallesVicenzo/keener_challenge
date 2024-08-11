1f6717319437daa68a99f747b4c1fd5d762c8ab7
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-08-11 13:54:29 -0300

    feat: create addtask external

---
+++ lib/features/add_task/external/add_task_datasource_impl.dart
@@ -0,0 +1,34 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:keener_challenge/core/domain/entities/task_entity.dart';
+import 'package:keener_challenge/core/infra/models/task_model.dart';
+import 'package:keener_challenge/core/secure_storage/keys/secure_storage_keys.dart';
+import 'package:keener_challenge/core/secure_storage/secure_storage.dart';
+import 'package:keener_challenge/features/add_task/infra/datasource/add_task_datasource.dart';
+
+class AddTaskDatasourceImpl implements AddTaskDatasource {
+  final FirebaseFirestore firestore;
+  final SecureStorage storage;
+
+  AddTaskDatasourceImpl({
+    required this.firestore,
+    required this.storage,
+  });
+
+  @override
+  Future<bool> addTask(TaskEntity entity) async {
+    final id = await storage.read(key: SecureStorageKeys.userId.key);
+
+    final value = TaskModel(
+      description: entity.description,
+      isCompleted: entity.isCompleted,
+      title: entity.title,
+    ).toJson();
+
+    await firestore.collection('tasks').doc(id).update(
+      {
+        'tasks': FieldValue.arrayUnion([value]),
+      },
+    );
+    return true;
+  }
+}
